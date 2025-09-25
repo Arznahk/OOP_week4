87fbdbb7e947d6255e5ae4b9c2efd7c8c2d0aa09
Arznahk <arznahk@gmail.com>
2025-09-25 18:06:55 +0900

    first update

---
+++ adding/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "math.cpp"
+
+int main(){
+    double a {};
+    double b {};
+    std::cout << "Enter the first number: " << std::endl;
+    std::cin >> a;
+    std::cout << "Enter the second number: " << std::endl;
+    std::cin >> b;
+    std::cout << "Added: " << add(a, b) << std::endl;
+    std::cout << "Multiplied: " << multiply(a, b) << std::endl;
+}
---
+++ adding/math.h
@@ -0,0 +1,7 @@
+int add(int, int);
+
+int multiply(int, int);
+
+int add(int, int, int);
+
+double add(double, double);
